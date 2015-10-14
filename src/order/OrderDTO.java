63a4573b674164ea944530bd5b325856d6c50ee5
MabinKim <zox0hey0jy2@naver.com>
2015-10-14 09:33:25 +0900

    test

---
+++ src/order/OrderDTO.java
@@ -0,0 +1,27 @@
+package order;
+
+public class OrderDTO {
+	
+	private int o_no;
+	private int o_mno;
+	private int o_pay;
+	
+	public int getO_no() {
+		return o_no;
+	}
+	public void setO_no(int o_no) {
+		this.o_no = o_no;
+	}
+	public int getO_mno() {
+		return o_mno;
+	}
+	public void setO_mno(int o_mno) {
+		this.o_mno = o_mno;
+	}
+	public int getO_pay() {
+		return o_pay;
+	}
+	public void setO_pay(int o_pay) {
+		this.o_pay = o_pay;
+	}
+}
---
+++ src/remark/RemarkDTO.java
@@ -0,0 +1,43 @@
+package remark;
+
+public class RemarkDTO {
+	
+	private int r_no;
+	private int r_pno;
+	private String r_title;
+	private String r_content;
+	private String r_writer;
+	
+	public int getR_no() {
+		return r_no;
+	}
+	public void setR_no(int r_no) {
+		this.r_no = r_no;
+	}
+	public int getR_pno() {
+		return r_pno;
+	}
+	public void setR_pno(int r_pno) {
+		this.r_pno = r_pno;
+	}
+	public String getR_title() {
+		return r_title;
+	}
+	public void setR_title(String r_title) {
+		this.r_title = r_title;
+	}
+	public String getR_content() {
+		return r_content;
+	}
+	public void setR_content(String r_content) {
+		this.r_content = r_content;
+	}
+	public String getR_writer() {
+		return r_writer;
+	}
+	public void setR_writer(String r_writer) {
+		this.r_writer = r_writer;
+	}
+	
+
+}
