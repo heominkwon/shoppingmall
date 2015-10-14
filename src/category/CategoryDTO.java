4848bcbf7606c566a8b5499bb456f5161cdb1aae
MabinKim <zox0hey0jy2@naver.com>
2015-10-14 09:33:25 +0900

    category 	            패키지 추가 order            패키지 추가 order_product    패키지 추가 remark           패키지 추가
    CategoryDTO      설계 및 구현
    OrderDTO         설계 및 구현
    Order_ProductDTO 설계 및 구현
    Remark_DTO       설계 및 구현

---
+++ src/category/CategoryDTO.java
@@ -0,0 +1,19 @@
+package category;
+
+public class CategoryDTO {
+	
+	private int c_no;
+	private String c_name;
+	public int getC_no() {
+		return c_no;
+	}
+	public void setC_no(int c_no) {
+		this.c_no = c_no;
+	}
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+}
---
+++ src/order_product/Order_ProductDTO.java
@@ -0,0 +1,43 @@
+package order_product;
+
+public class Order_ProductDTO {
+
+	private int op_no;
+	private int op_ono;
+	private int op_pno;
+	private int op_count;
+	
+	
+	public int getOp_no() {
+		return op_no;
+	}
+	public void setOp_no(int op_no) {
+		this.op_no = op_no;
+	}
+	public int getOp_ono() {
+		return op_ono;
+	}
+	public void setOp_ono(int op_ono) {
+		this.op_ono = op_ono;
+	}
+	public int getOp_pno() {
+		return op_pno;
+	}
+	public void setOp_pno(int op_pno) {
+		this.op_pno = op_pno;
+	}
+	public int getOp_count() {
+		return op_count;
+	}
+	public void setOp_count(int op_count) {
+		this.op_count = op_count;
+	}
+	public int getOp_price() {
+		return op_price;
+	}
+	public void setOp_price(int op_price) {
+		this.op_price = op_price;
+	}
+	private int op_price;
+
+}
