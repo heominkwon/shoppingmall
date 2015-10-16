38cce380f2883bd163769d4ac37dd2ecbca6052d
kimmabin <d_loko@naver.com>
2015-10-16 11:53:29 +0900

    CategoryInterface 설계 CategoryDAO 설계

--- src/category/CategoryDAO.java
+++ src/category/CategoryDAO.java
@@ -1,5 +1,72 @@
 package category;
 
-public class CategoryDAO {
+import java.sql.Connection;
+
+public class CategoryDAO implements CategoryInterface{
+
+	@Override
+	public Connection getConnection() throws Exception {
+		return null;
+	}
+
+	@Override
+	public void insertCategory(CategoryDTO category) throws Exception {
+		
+	}
+
+	@Override
+	public void deleteCategory(CategoryDTO category) throws Exception {
+		
+	}
+
+	@Override
+	public void updateCategory(CategoryDTO category) throws Exception {
+		
+	}
+
+	@Override
+	public CategoryDTO selectCategory(CategoryDTO category) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void deleteCategory(int categoryNO) throws Exception {
+		
+	}
+
+	@Override
+	public void deleteCategory(String categoryNAme) throws Exception {
+		
+	}
+
+	@Override
+	public void updateCategory(int categoryNO, int change_categoryNO) throws Exception {
+		
+	}
+
+	@Override
+	public void updateCategory(int categoryNO, String change_categoryName) throws Exception {
+		
+	}
+
+	@Override
+	public void updateCategory(String categoryName, int change_categoryNO) throws Exception {
+		
+	}
+
+	@Override
+	public void updateCategory(String categoryName, String change_categoryName) throws Exception {
+		
+	}
+
+	@Override
+	public CategoryDTO selectCategory(int categoryNO) throws Exception {
+		return null;
+	}
+
+	@Override
+	public CategoryDTO selectCategory(String categoryName) throws Exception {
+		return null;
+	}
 
 }
--- src/category/CategoryInterface.java
+++ src/category/CategoryInterface.java
@@ -1,5 +1,33 @@
 package category;
 
+import java.sql.Connection;
+
 public interface CategoryInterface {
+
+	Connection getConnection() throws Exception;
+
+	void insertCategory(CategoryDTO category) throws Exception;
+
+	void deleteCategory(CategoryDTO category) throws Exception;
+	
+	void updateCategory(CategoryDTO category) throws Exception;
+
+	CategoryDTO selectCategory(CategoryDTO category) throws Exception;
+	
+	void deleteCategory(int categoryNO) throws Exception;
+	
+	void deleteCategory(String categoryNAme) throws Exception;
+	
+	void updateCategory(int categoryNO, int change_categoryNO) throws Exception;
+	
+	void updateCategory(int categoryNO, String change_categoryName) throws Exception;
+	
+	void updateCategory(String categoryName, int change_categoryNO) throws Exception;
+	
+	void updateCategory(String categoryName, String change_categoryName) throws Exception;
+	
+	CategoryDTO selectCategory(int categoryNO) throws Exception;
+	
+	CategoryDTO selectCategory(String categoryName) throws Exception;
 
 }
