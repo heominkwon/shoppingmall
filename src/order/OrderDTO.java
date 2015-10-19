64716bcc1d5420c43dc816a04a165f5530168e5e
MabinKim <zox0hey0jy2@naver.com>
2015-10-19 11:13:14 +0900

    OrderDTO  클래스 메소드 추가 	public String Str_getO_no() 	public String Str_getO_mno() 	public String Str_getO_pay() 	public void setO_no(String o_no) 	public void setO_mno(String o_mno) 	public void setO_pay(String o_pay)

--- src/order/OrderDTO.java
+++ src/order/OrderDTO.java
@@ -9,19 +9,40 @@ public class OrderDTO {
 	public int getO_no() {
 		return o_no;
 	}
-	public void setO_no(int o_no) {
-		this.o_no = o_no;
-	}
 	public int getO_mno() {
 		return o_mno;
-	}
-	public void setO_mno(int o_mno) {
-		this.o_mno = o_mno;
 	}
 	public int getO_pay() {
 		return o_pay;
 	}
+	
+	public String Str_getO_no() {
+		return String.valueOf(o_no);
+	}
+	public String Str_getO_mno() {
+		return String.valueOf(o_mno);
+	}
+	public String Str_getO_pay() {
+		return String.valueOf(o_pay);
+	}
+	
+	public void setO_no(int o_no) {
+		this.o_no = o_no;
+	}
+	public void setO_mno(int o_mno) {
+		this.o_mno = o_mno;
+	}
 	public void setO_pay(int o_pay) {
 		this.o_pay = o_pay;
+	}
+	
+	public void setO_no(String o_no) {
+		this.o_no = Integer.parseInt(o_no);
+	}
+	public void setO_mno(String o_mno) {
+		this.o_mno = Integer.parseInt(o_mno);
+	}
+	public void setO_pay(String o_pay) {
+		this.o_pay = Integer.parseInt(o_pay);
 	}
 }
