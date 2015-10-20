a6d608792e6add952c70f19e55f458456a7ed8e0
MabinKim <zox0hey0jy2@naver.com>
2015-10-20 13:04:47 +0900

    ProductDAO 구현 Connection getConnection() void insertProduct(ProductDTO product) void deleteProduct(ProductDTO product) void updateProduct(ProductDTO product) ProductDTO selectProduct(ProductDTO product)
    void deleteProduct(int NEED_productNO)
    void deleteProduct(String NEED_productNAME)
    
    void updateNO_NO(int NEED_productNO,int change_productNO)
    void updateNO_CNO(int NEED_categoryNO,int change_productNO)
    void updateNO_NAME(String NEED_productNAME,int change_productNO)
    
    void updateCNO_NO(int NEED_productNO,int change_categoryNO)
    void updateCNO_CNO(int NEED_categoryNO,int change_categoryNO)
    void updateCNO_NAME(String NEED_productNAME,int change_categoryNO)
    
    void updateName_NO(int NEED_productNO, String change_productName)
    void updateName_CNO(int NEED_categoryNO, String change_productName)
    void updateName_NAME(String NEED_productName, String change_productName)
    
    void updatePrice_NO(int NEED_productNO, int change_price) throw
    void updatePrice_NAME(String NEED_productName, int change_price)
    
    void updateCount_NO(int NEED_productNO, int change_count) throws
    void updateCount_NAME(String NEED_productNAME, int change_count)
    
    void updateDesc_NO(int NEED_productNO, String change_desc) throws
    void updateDesc_NAME(String NEED_productNAME, String change_desc)
    
    void updatePath_NO(int NEED_productNO, String change_path)
    void updatePath_NAME(String NEED_productNAME, String change_path)
    
    ProductDTO selectProduct(int NEED_productNO)
    ProductDTO selectProduct(String NEED_productNAME)
    
    List<ProductDTO> selectsProduct_CNO(int NEED_categoryNO)

--- src/product/ProductDAO.java
+++ src/product/ProductDAO.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -38,8 +39,8 @@ public class ProductDAO implements ProductInterface{
 		try {
 			conn  = getConnection();
 			pstmt = conn.prepareStatement(
-					"INSERT INTO PRODUCT"
-					+ "(P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE)"
+					"INSERT INTO PRODUCT "
+					+ "(P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE) "
 					+ " VALUES "
 					+ "(PRODUCT_SEQ.NEXTVAL,?,?,?,?,?,?,?)");
 			pstmt.setInt(1, product.getP_cno());
@@ -93,7 +94,7 @@ public class ProductDAO implements ProductInterface{
 			pstmt = conn.prepareStatement(
 					"UPDATE "
 					+ "PRODUCT "
-					+ "SET P_CNO=?, P_NAME=?, P_PRICE=?, P_COUNT=?, P_DESC, P_PATH, P_REGDATE"
+					+ "SET P_CNO=?, P_NAME=?, P_PRICE=?, P_COUNT=?, P_DESC, P_PATH, P_REGDATE "
 					+ "WHERE P_NO=?");
 			pstmt.setInt(1, product.getP_cno());
 			pstmt.setString(2, product.getP_name());
@@ -127,7 +128,7 @@ public class ProductDAO implements ProductInterface{
 			conn  = getConnection();
 			pstmt = conn.prepareStatement(
 					"SELECT "
-					+ "P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE"
+					+ "P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE "
 					+ "FROM PRODUCT WHERE P_NO=?");
 			pstmt.setInt(1, product.getP_no());
 			
@@ -155,8 +156,143 @@ public class ProductDAO implements ProductInterface{
 		return dto;
 	}
 
+	
+	
 	@Override
-	public void updateCNO_NO(String NEED_productNO, int change_categoryNO) throws Exception{
+	public void deleteProduct(int NEED_productNO) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(""
+					+ "DELETE "
+					+ "FROM PRODUCT "
+					+ "WHERE P_NO=?");
+			pstmt.setInt(1, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+
+	@Override
+	public void deleteProduct(String NEED_productNAME) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(""
+					+ "DELETE "
+					+ "FROM PRODUCT "
+					+ "WHERE P_NAME=?");
+			pstmt.setString(1, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}	
+	}
+	
+	
+	
+	@Override
+	public void updateNO_NO(int NEED_productNO, int change_productNO) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_NO=? "
+					+ "WHERE P_NO=?");
+			pstmt.setInt(1, change_productNO);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+		
+	
+
+	@Override
+	public void updateNO_CNO(int NEED_categoryNO, int change_productNO) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_NO=? "
+					+ "WHERE P_CNO=?");
+			pstmt.setInt(1, change_productNO);
+			pstmt.setInt(2, NEED_categoryNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+		
+	}
+
+	@Override
+	public void updateNO_NAME(String NEED_productNAME, int change_productNO) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_NO=? "
+					+ "WHERE P_NAME=?");
+			pstmt.setInt(1, change_productNO);
+			pstmt.setString(2, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+	
+	
+	
+	@Override
+	public void updateCNO_NO(int NEED_productNO, int change_categoryNO) throws Exception{
 		
 		Connection		  conn  = null;
 		PreparedStatement pstmt = null;
@@ -166,10 +302,36 @@ public class ProductDAO implements ProductInterface{
 			pstmt = conn.prepareStatement(
 					"UODATE "
 					+ "PRODUCT "
-					+ "SET P_CNO=?"
+					+ "SET P_CNO=? "
 					+ "WHERE P_NO=?");
 			pstmt.setInt(1, change_categoryNO);
-			pstmt.setString(2, NEED_productNO);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null)  try {conn.close();} catch (SQLException se) {}
+		}
+	}
+	
+	@Override
+	public void updateCNO_CNO(int NEED_categoryNO, int change_categoryNO) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UODATE "
+					+ "PRODUCT "
+					+ "SET P_CNO=? "
+					+ "WHERE P_CNO=?");
+			pstmt.setInt(1, change_categoryNO);
+			pstmt.setInt(2, NEED_categoryNO);
 			
 			pstmt.executeUpdate();
 			
@@ -182,6 +344,86 @@ public class ProductDAO implements ProductInterface{
 	}
 
 	@Override
+	public void updateCNO_NAME(String NEED_productNAME, int change_categoryNO) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UODATE "
+					+ "PRODUCT "
+					+ "SET P_CNO=? "
+					+ "WHERE P_NAME=?");
+			pstmt.setInt(1, change_categoryNO);
+			pstmt.setString(2, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null)  try {conn.close();} catch (SQLException se) {}
+		}
+	}
+
+	
+	
+	@Override
+	public void updateName_NO(int NEED_productNO, String change_productName) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_NAME=? "
+					+ "WHERE P_NO");
+			pstmt.setString(1, change_productName);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+
+	@Override
+	public void updateName_CNO(int NEED_categoryNO, String change_productName) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_NAME=? "
+					+ "WHERE P_CNO");
+			pstmt.setString(1, change_productName);
+			pstmt.setInt(2, NEED_categoryNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+	
+	@Override
 	public void updateName_NAME(String NEED_productName, String change_productName)  throws Exception{
 		
 		Connection 		  conn  = null;
@@ -192,7 +434,7 @@ public class ProductDAO implements ProductInterface{
 			pstmt = conn.prepareStatement(
 					"UPDATE "
 					+ "PRODUCT "
-					+ "SET P_NAME=?"
+					+ "SET P_NAME=? "
 					+ "WHERE P_NAME=?");
 			pstmt.setString(1, change_productName);
 			pstmt.setString(2, NEED_productName);
@@ -207,8 +449,10 @@ public class ProductDAO implements ProductInterface{
 		}
 	}
 
+	
+	
 	@Override
-	public void updatePrice_NO(String NEED_productName, int change_price)  throws Exception{
+	public void updatePrice_NO(int NEED_productNO, int change_price)  throws Exception{
 		
 		Connection 		  conn  = null;
 		PreparedStatement pstmt = null;
@@ -218,7 +462,33 @@ public class ProductDAO implements ProductInterface{
 			pstmt = conn.prepareStatement(
 					"UPDATE "
 					+ "PRODUCT "
-					+ "SET P_PRICE=?"
+					+ "SET P_PRICE=? "
+					+ "WHERE P_NO=?");
+			pstmt.setInt(1, change_price);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+	
+	@Override
+	public void updatePrice_NAME(String NEED_productName, int change_price) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_PRICE=? "
 					+ "WHERE P_NAME=?");
 			pstmt.setInt(1, change_price);
 			pstmt.setString(2, NEED_productName);
@@ -233,8 +503,10 @@ public class ProductDAO implements ProductInterface{
 		}
 	}
 
+	
+	
 	@Override
-	public void updateCount_NO(String NEED_productName, int change_count) throws Exception{
+	public void updateCount_NO(int NEED_productNO, int change_count) throws Exception{
 		
 		Connection		  conn  = null;
 		PreparedStatement pstmt = null;
@@ -245,9 +517,9 @@ public class ProductDAO implements ProductInterface{
 					"UPDATE "
 					+ "PRODUCT "
 					+ "SET P_COUNT=?"
-					+ "WHER P_NAME=?");
+					+ "WHERE P_NO=?");
 			pstmt.setInt(1, change_count);
-			pstmt.setString(2, NEED_productName);
+			pstmt.setInt(2, NEED_productNO);
 			
 			pstmt.executeUpdate();
 	
@@ -257,5 +529,264 @@ public class ProductDAO implements ProductInterface{
 			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
 			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
 		}
+	}
+	
+	@Override
+	public void updateCount_NAME(String NEED_productNAME, int change_count) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_COUNT=? "
+					+ "WHERE P_NAME=?");
+			pstmt.setInt(1, change_count);
+			pstmt.setString(2, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+	
+	
+	
+	@Override
+	public void updateDesc_NO(int NEED_productNO, String change_desc) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_DESC=? "
+					+ "WHERE P_NO=?");
+			pstmt.setString(1, change_desc);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+
+	@Override
+	public void updateDesc_NAME(String NEED_productNAME, String change_desc) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_DESC=? "
+					+ "WHERE P_NAME=?");
+			pstmt.setString(1, change_desc);
+			pstmt.setString(2, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+
+	
+	
+	@Override
+	public void updatePath_NO(int NEED_productNO, String change_path) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_PATH=? "
+					+ "WHERE P_NO=?");
+			pstmt.setString(1, change_path);
+			pstmt.setInt(2, NEED_productNO);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+
+	@Override
+	public void updatePath_NAME(String NEED_productNAME, String change_path) throws Exception {
+		
+		Connection 		  conn  = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"UPDATE "
+					+ "PRODUCT "
+					+ "SET P_PATH=? "
+					+ "WHERE P_NAME=?");
+			pstmt.setString(1, change_path);
+			pstmt.setString(2, NEED_productNAME);
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+	}
+	
+	
+	
+
+	@Override
+	public ProductDTO selectProduct(int NEED_productNO) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet 		  rs    = null;
+		ProductDTO 		  dto   = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"SELECT "
+					+ "P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE "
+					+ "FROM PRODUCT "
+					+ "WHERE P_NO=?");
+			pstmt.setInt(1, NEED_productNO);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				dto = new ProductDTO();
+				dto.setP_no(rs.getInt(1));
+				dto.setP_cno(rs.getInt(2));
+				dto.setP_name(rs.getString(3));
+				dto.setP_price(rs.getInt(4));
+				dto.setP_count(rs.getInt(5));
+				dto.setP_desc(rs.getString(6));
+				dto.setP_path(rs.getString(7));
+				dto.setP_regdate(rs.getTimestamp(8));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs    != null) try {rs.close();}    catch (SQLException se) {}
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+		return dto;
+	}
+	
+
+	@Override
+	public ProductDTO selectProduct(String NEED_productNAME) throws Exception {
+		
+		Connection		  conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet 		  rs    = null;
+		ProductDTO 		  dto   = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"SELECT "
+					+ "P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE "
+					+ "FROM PRODUCT "
+					+ "WHERE P_NAME=?");
+			pstmt.setString(1, NEED_productNAME);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				dto = new ProductDTO();
+				dto.setP_no(rs.getInt(1));
+				dto.setP_cno(rs.getInt(2));
+				dto.setP_name(rs.getString(3));
+				dto.setP_price(rs.getInt(4));
+				dto.setP_count(rs.getInt(5));
+				dto.setP_desc(rs.getString(6));
+				dto.setP_path(rs.getString(7));
+				dto.setP_regdate(rs.getTimestamp(8));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs    != null) try {rs.close();}    catch (SQLException se) {}
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn  != null) try {conn.close();}  catch (SQLException se) {}
+		}
+		return dto;
+	}
+
+	@Override
+	public List<ProductDTO> selectsProduct_CNO(int NEED_categoryNO) throws Exception {
+		
+		Connection		  conn    = null;
+		PreparedStatement pstmt   = null;
+		ResultSet		  rs      = null;
+		List<ProductDTO>  dtolist = null;
+		
+		try {
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(
+					"SELECT "
+					+ "P_NO, P_CNO, P_NAME, P_PRICE, P_COUNT, P_DESC, P_PATH, P_REGDATE "
+					+ "FROM PRODUCT "
+					+ "WHERE P_CNO=?");
+			pstmt.setInt(1, NEED_categoryNO);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				ProductDTO dto = new ProductDTO();
+				dto.setP_no(rs.getInt(1));
+				dto.setP_cno(rs.getInt(2));
+				dto.setP_name(rs.getString(3));
+				dto.setP_price(rs.getInt(4));
+				dto.setP_count(rs.getInt(5));
+				dto.setP_desc(rs.getString(6));
+				dto.setP_path(rs.getString(7));
+				dto.setP_regdate(rs.getTimestamp(8));
+				dtolist.add(dto);
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}finally {
+			if (rs != null)    try {rs.close();}    catch (SQLException se) {}
+			if (pstmt != null) try {pstmt.close();} catch (SQLException se) {}
+			if (conn != null)  try {conn.close();}  catch (SQLException se) {}
+		}
+		return dtolist;
 	}
 }
