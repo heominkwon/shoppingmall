eec3a4e3f4965f62168c5be4f2b18af07c6b1ae2
MabinKim <zox0hey0jy2@naver.com>
2015-10-21 10:46:30 +0900

    RemarkDAO Basic Method Change

--- src/remark/RemarkDAO.java
+++ src/remark/RemarkDAO.java
@@ -16,19 +16,21 @@ public class RemarkDAO implements RemarkInterface{
 		Context initCtx = new InitialContext();
 		Context envCtx = (Context)initCtx.lookup("java:/comp/env");
 		DataSource ds = (DataSource)envCtx.lookup("jdbc/orcl");
-		return null;
+		return ds.getConnection();
 	}
 
 	@Override
 	public void insertRemark(RemarkDTO remark) throws Exception {
-		Connection    		conn 	= null;
+		Connection    		 conn 	= null;
 		PreparedStatement    pstmt	= null;
 		
 		try{
 			conn = getConnection();
-			pstmt= conn.prepareStatement(
-					"INSERT INTO REMARK VALUES("
-					+ "REMARK_SEQ.NEXTVAL,?,?,?,?)");
+			pstmt= conn.prepareStatement(""
+					+ "INSERT INTO REMARK "
+					+ "(R_NO, R_PNO, R_TITLE, R_CONTENT, R_WRITER) "
+					+ "VALUES "
+					+ "(REMARK_SEQ.NEXTVAL,?,?,?,?)");
 			pstmt.setInt(1,remark.getR_pno());
 			pstmt.setString(2, remark.getR_title());
 			pstmt.setString(3, remark.getR_content());
@@ -39,25 +41,22 @@ public class RemarkDAO implements RemarkInterface{
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
-			if(pstmt!=null)try {pstmt.close();} catch (SQLException se) {}
-			if(conn!=null)try {conn.close();} catch (SQLException se) {}
+			if(pstmt!=null) try {pstmt.close();} catch (SQLException se) {}
+			if(conn !=null) try {conn.close();}  catch (SQLException se) {}
 		}
 	}
 
 	@Override
 	public void deleteRemark(RemarkDTO remark) throws Exception {
 		
-		Connection        conn = null;
+		Connection        conn  = null;
 		PreparedStatement pstmt = null;
 		
 		try{
-			conn = getConnection();
+			conn  = getConnection();
 			pstmt = conn.prepareStatement(
-					"DELETE FROM PRODUCT WHERE P_NAME = ?");
-			pstmt.setInt(1, remark.getR_pno());
-			pstmt.setString(2, remark.getR_title());
-			pstmt.setString(3, remark.getR_content());
-			pstmt.setString(4, remark.getR_writer());
+					"DELETE FROM REMARK WHERE R_NO=?");
+			pstmt.setInt(1, remark.getR_no());
 			
 			pstmt.executeUpdate();
 		
@@ -65,7 +64,7 @@ public class RemarkDAO implements RemarkInterface{
 			e.printStackTrace();
 		} finally {
 			if(pstmt != null) try{pstmt.close();} catch(SQLException se) {}
-			if(conn != null) try{conn.close();} catch(SQLException se) {}
+			if(conn  != null) try{conn.close();}  catch(SQLException se) {}
 		}
 	}
 
@@ -76,53 +75,63 @@ public class RemarkDAO implements RemarkInterface{
 		PreparedStatement   pstmt = null; 
 		
 		try {
-			conn = getConnection();
+			conn  = getConnection();
 			pstmt = conn.prepareStatement(
-					"UPDATE REMARK SET R_PNO=?, R_TITLE=? , R_CONTENT=? , R_WRITER=?");
+					"UPDATE "
+					+ "REMARK "
+					+ "SET R_PNO=?, R_TITLE=? , R_CONTENT=? , R_WRITER=? "
+					+ "WHERE R_NO=?");
 			pstmt.setInt(1, remark.getR_pno());
 			pstmt.setString(2, remark.getR_title());
 			pstmt.setString(3, remark.getR_content());
 			pstmt.setString(4, remark.getR_writer());
+			pstmt.setInt(5, remark.getR_no());
+			
 			pstmt.executeUpdate();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if(pstmt != null) try{pstmt.close();} catch(SQLException se) {}
-			if(conn != null) try{conn.close();} catch(SQLException se) {}
+			if(conn  != null) try{conn.close();}  catch(SQLException se) {}
 		}
 	}
 
 
 	@Override
 	public RemarkDTO selectRemark(RemarkDTO remark) throws Exception {
-			Connection    		conn = null;
-			PreparedStatement  pstmt = null;
-			ResultSet            rs  = null;
-			RemarkDTO			dto  = null;
+			Connection    		conn  = null;
+			PreparedStatement   pstmt = null;
+			ResultSet           rs    = null;
+			RemarkDTO			dto   = null;
 		
 			try{
 				conn = getConnection();
 				pstmt = conn.prepareStatement(
-						"SELECT * FROM REMARK WHERE R_PNO = ?");
-				pstmt.setInt(1,remark.getR_pno());
+						"SELECT "
+						+ "R_NO, R_PNO, R_TITLE, R_CONTENT, R_WRITER "
+						+ "FROM REMARK "
+						+ "WHERE R_NO=?");
+				
+				pstmt.setInt(1,remark.getR_no());
 				
 				rs = pstmt.executeQuery();
 				
 				while (rs.next()) {
 					dto = new RemarkDTO();
-					dto.setR_pno(rs.getInt(1));
-					dto.setR_title(rs.getString(2));
-					dto.setR_content(rs.getString(3));
-					dto.setR_writer(rs.getString(4));
+					dto.setR_no(rs.getInt(1));
+					dto.setR_pno(rs.getInt(2));
+					dto.setR_title(rs.getString(3));
+					dto.setR_content(rs.getString(4));
+					dto.setR_writer(rs.getString(5));
 				}
 			
 			}catch(Exception e) {
 				e.printStackTrace();
 			}finally {
-				if(rs  !=null) try{rs.close();} catch(SQLException se){}
-				if(pstmt  !=null) try{pstmt.close();} catch(SQLException se){}
-				if(conn  !=null) try{conn.close();} catch(SQLException se){}
+				if(rs    !=null) try{rs.close();}    catch(SQLException se){}
+				if(pstmt !=null) try{pstmt.close();} catch(SQLException se){}
+				if(conn  !=null) try{conn.close();}  catch(SQLException se){}
 			}
 			return dto;
 	}
