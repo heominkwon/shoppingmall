a104d39f5ce1f7db75436e29060cc8e81e90f836
kimmabin <d_loko@nave.com>
2015-10-31 09:00:46 +0900

    Revert "Revert "CategoryInterface CategoryDAO Refactoring""
    This reverts commit 54149870dd49cc8b9e8a0cb8270dba605bb05437.

--- src/category/CategoryInterface.java
+++ src/category/CategoryInterface.java
@@ -8,22 +8,21 @@ public interface CategoryInterface {
 	Connection getConnection() throws Exception;
 
 	void insertCategory(CategoryDTO category) throws Exception;
-	void deleteCategory(CategoryDTO category) throws Exception;
-	void updateCategory(CategoryDTO category) throws Exception;
 
-	CategoryDTO selectCategory(CategoryDTO category) throws Exception;
-	
 	void deleteCategory(int c_no) throws Exception;
 	void deleteCategory(String c_name) throws Exception;
-	
+
 	void updateCategory(int c_no, int updateC_no) throws Exception;
-	void updateCategory(int c_no, String updateC_name) throws Exception;
 	void updateCategory(String c_name, int updateC_no) throws Exception;
+	void updateCategory(int c_no, String updateC_name) throws Exception;
 	void updateCategory(String c_name, String updateC_name) throws Exception;
 	
 	CategoryDTO selectCategory(int c_no) throws Exception;
 	CategoryDTO selectCategory(String c_name) throws Exception;
 	
 	List<Integer> selectAllC_no() throws Exception;
+	List<Integer> selectAllC_name() throws Exception;
+	
+	List<CategoryDTO> selectAll() throws Exception;
 
 }
